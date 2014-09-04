82c003c2a72b241ee22ba4566a86e999b3bca800
Sankalp Kulshreshtha <sankalp221@gmail.com>
2014-09-04 19:57:19 -0400

    Added content for videos

--- js/angular.main.js
+++ js/angular.main.js
@@ -18,6 +18,14 @@ jadyuApp.factory('Data', function(){
             interlinks:
             [
                 {
+                    start: 13,
+                    target: "caption",
+                    title: '<a href="#/peer-to-peer">Peer-to-peer</a>',
+                    text:"",
+                    innerHTML:"",
+                    direction: "up"
+                },
+                {
                     start: 28,
                     target: "caption",
                     title: '<a href="#/bitcoin-exchange">Bitcoin Exchange</a>',
@@ -36,7 +44,7 @@ jadyuApp.factory('Data', function(){
                 {
                     start: 42,
                     target: "caption",
-                    title: '<a ng-click="changeTopic(2)">Bitcoin Merchant</a>',
+                    title: '<a href="#/bitcoin-merchant">Bitcoin Merchant</a>',
                     text: "",
                     innerHTML: "",
                     direction: "up"
@@ -44,7 +52,7 @@ jadyuApp.factory('Data', function(){
                 {   
                     start: 47,
                     target: "caption",
-                    title: '<a ng-click="changeTopic(2)">Bitcoin Miner</a>',
+                    title: '<a href="#/bitcoin-mining">Bitcoin Mining</a>',
                     text: "",
                     innerHTML: "",
                     direction: "up"
@@ -52,7 +60,7 @@ jadyuApp.factory('Data', function(){
                 {
                     start: 57,
                     target: "caption",
-                    title: '<a ng-click="changeTopic(2)">Bitcoin Blockchain</a>',
+                    title: '<a href="#/bitcoin-blockchain">Bitcoin Blockchain</a>',
                     text: "",
                     innerHTML: "",
                     direction: "up"
@@ -72,7 +80,7 @@ jadyuApp.factory('Data', function(){
                 {
                     start: 106,
                     target: 'caption',
-                    title: '<a ng-click="changeTopic(1)">Bitcoin</a>',
+                    title: '<a href="#/bitcoin">Bitcoin</a>',
                     text: "",
                     innerHTML: "",
                     direction:"up"
@@ -80,14 +88,358 @@ jadyuApp.factory('Data', function(){
                 {                
                     start: 110,
                     target: 'caption',
-                    title: '<a ng-click="changeTopic(1)">Altcoin</a>',
+                    title: '<a href="#/altcoin">Altcoin</a>',
                     text: "",
                     innerHTML: "",
                     direction:"up"
 
                 }
             ]
+        },
+        {
+            id: 3,   
+            title: "Bitcoin Wallet",
+            startTime:0,
+            endTime:96,
+            videoId: "oTfOfqmb5tU",
+            type: "YouTube",
+            path: "bitcoin-wallet",
+            interlinks:
+            [
+                {
+                    start: 11,
+                    target: 'caption',
+                    title: '<a href="#/bitcoin-address">Bitcoin Address</a>',
+                    text: "",
+                    innerHTML: "",
+                    direction:"up"
+                },
+                {                
+                    start: 32,
+                    target: 'caption',
+                    title: '<a href="#/bitcoin-wallet">Bitcoin Wallet</a>',
+                    text: "",
+                    innerHTML: "",
+                    direction:"up"
+
+                },
+                {
+                    start: 40,
+                    target: 'caption',
+                    title: '<a href="#/private-key">Private Key</a>',
+                    text: "",
+                    innerHTML: "",
+                    direction:"up"
+                },
+                {
+                    start: 75,
+                    target: 'caption',
+                    title: '<a href="#/brain-wallet">Brain Wallet</a>',
+                    text: "",
+                    innerHTML: "",
+                    direction:"up"
+                }
+            ]
+        },
+        {
+            id: 4,   
+            title: "Bitcoin Merchant",
+            startTime:696,
+            endTime:747,
+            videoId: "8Z3-DA7aFCU",
+            type: "YouTube",
+            path: "bitcoin-merchant",
+            interlinks:
+            [
+
+                {
+                    start: 704,
+                    target: 'caption',
+                    title: '<a href="#/bitcoin">Bitcoin</a>',
+                    text: "",
+                    innerHTML: "",
+                    direction:"up"
+                }
+            ]
+        },
+        {
+            id: 5,   
+            title: "Bitcoin Mining",
+            startTime:0,
+            endTime:115,
+            videoId: "GmOzih6I1zs",
+            type: "YouTube",
+            path: "bitcoin-mining",
+            interlinks:
+            [
+                {
+                    start: 3,
+                    target: 'caption',
+                    title: '<a href="#/peer-to-peer">Peer-to-peer</a>',
+                    text: "",
+                    innerHTML: "",
+                    direction:"up"
+                },
+                {
+                    start: 90,
+                    target: 'caption',
+                    title: '<a href="#/bitcoin-pool">Bitcoin Pool</a>',
+                    text: "",
+                    innerHTML: "",
+                    direction:"up"
+                }
+            ]
+        },
+        {
+            id: 6,   
+            title: "Bitcoin Blockchain",
+            startTime:13,
+            endTime:118,
+            videoId: "1tVaA57sP9k",
+            type: "YouTube",
+            path: "bitcoin-blockchain",
+            interlinks:
+            [
+                {
+                    start: 18,
+                    target: 'caption',
+                    title: '<a href="#/peer-to-peer">Peer-to-peer</a>',
+                    text: "",
+                    innerHTML: "",
+                    direction:"up"
+                },
+                {
+                    start: 105,
+                    target: 'caption',
+                    title: '<a href="#/transaction-fee">Bitcoin Transaction Fee</a>',
+                    text: "",
+                    innerHTML: "",
+                    direction:"up"
+                }
+            ]
+        },
+        {
+            id: 7,   
+            title: "Altcoin",
+            startTime:154,
+            endTime:306,
+            videoId: "VOICfyCvous",
+            type: "YouTube",
+            path: "altcoin",
+            interlinks:
+            [
+                {
+                    start: 165,
+                    target: 'caption',
+                    title: '<a href="#/litecoin">Litecoin</a>',
+                    text: "",
+                    innerHTML: "",
+                    direction:"up"
+                },
+                {
+                    start: 173,
+                    target: 'caption',
+                    title: '<a href="#/bitcoin">Bitcoin</a>',
+                    text: "",
+                    innerHTML: "",
+                    direction:"up"
+                },
+                {
+                    start: 186,
+                    target: 'caption',
+                    title: '<a href="#/premine">Premine</a>',
+                    text: "",
+                    innerHTML: "",
+                    direction:"up"
+                },
+                {                
+                    start: 236,
+                    target: 'caption',
+                    title: '<a href="#/bitcoin-mining">Bitcoin Mining</a>',
+                    text: "",
+                    innerHTML: "",
+                    direction:"up"
+
+                },
+                {
+                    start: 287,
+                    target: 'caption',
+                    title: '<a href="#/bitcoin-exchange">Bitcoin Exchange</a>',
+                    text: "",
+                    innerHTML: "",
+                    direction:"up"
+                }
+            ]
+        },
+        {
+            id: 8,   
+            title: "Bitcoin Address",
+            startTime:164,
+            endTime:180,
+            videoId: "Lx9zgZCMqXE",
+            type: "YouTube",
+            path: "bitcoin-address",
+            interlinks:
+            [
+                {
+                    start: 166,
+                    target: 'caption',
+                    title: '<a href="#/bitcoin">Bitcoin</a>',
+                    text: "",
+                    innerHTML: "",
+                    direction:"up"
+                },
+                {
+                    start: 179,
+                    target: 'caption',
+                    title: '<a href="#/private-key">Private Key</a>',
+                    text: "",
+                    innerHTML: "",
+                    direction:"up"
+                }
+            ]
+        },
+        {
+            id: 9,   
+            title: "Private Key",
+            startTime:6,
+            endTime:88,
+            videoId: "oaG-XJlvFSc",
+            type: "YouTube",
+            path: "private-key",
+            interlinks:
+            [
+                {
+                    start: 12,
+                    target: 'caption',
+                    title: '<a href="#/bitcoin-wallet">Bitcoin Wallet</a>',
+                    text: "",
+                    innerHTML: "",
+                    direction:"up"
+                },
+                {
+                    start: 16,
+                    target: 'caption',
+                    title: '<a href="#/bitcoin-address">Bitcoin Address</a>',
+                    text: "",
+                    innerHTML: "",
+                    direction:"up"
+                },
+                {
+                    start: 23,
+                    target: 'caption',
+                    title: '<a href="#/bitcoin">Bitcoin</a>',
+                    text: "",
+                    innerHTML: "",
+                    direction:"up"
+                },
+                {
+                    start: 27,
+                    target: 'caption',
+                    title: '<a href="#/bitcoin-blockchain">Bitcoin Blockchain</a>',
+                    text: "",
+                    innerHTML: "",
+                    direction:"up"
+                },
+                {
+                    start: 32,
+                    target: 'caption',
+                    title: '<a href="#/peer-to-peer">Peer-to-peer</a>',
+                    text: "",
+                    innerHTML: "",
+                    direction:"up"
+                }
+            ]
+        },
+       {
+            id: 10,   
+            title: "Brain Wallet",
+            startTime:5,
+            endTime:68,
+            videoId: "fBOWAqmS7qY",
+            type: "YouTube",
+            path: "brain-wallet",
+            interlinks:
+            [
+                {
+                    start: 62,
+                    target: 'caption',
+                    title: '<a href="#/private-key">Private Key</a>',
+                    text: "",
+                    innerHTML: "",
+                    direction:"up"
+                }
+            ]
+        },
+        {
+            id: 11,   
+            title: "Peer-to-peer",
+            startTime:125,
+            endTime:228,
+            videoId: "C5UWEWcGJgs",
+            type: "YouTube",
+            path: "peer-to-peer",
+            interlinks:
+            [
+                {
+                    start: 190,
+                    target: 'caption',
+                    title: '<a href="#/bitcoin">Bitcoin</a>',
+                    text: "",
+                    innerHTML: "",
+                    direction:"up"
+                }
+            ]
+        },
+        {
+            id: 12,   
+            title: "Bitcoin Pool",
+            startTime:133,
+            endTime:177,
+            videoId: "zXmIeOL0QPg",
+            type: "YouTube",
+            path: "bitcoin-pool",
+            interlinks:
+            [
+                {
+                    start: 141,
+                    target: 'caption',
+                    title: '<a href="#/bitcoin-blockchain">Bitcoin Blockchain</a>',
+                    text: "",
+                    innerHTML: "",
+                    direction:"up"
+                }
+            ]
+        },
+        {
+            id: 13,   
+            title: "Transaction Fee",
+            startTime:375,
+            endTime:426,
+            videoId: "Em8nJN8IEes",
+            type: "YouTube",
+            path: "transaction-fee",
+            interlinks:
+            [
+                {
+                    start: 414,
+                    target: 'caption',
+                    title: '<a href="#/bitcoin-blockchain">Bitcoin Blockchain</a>',
+                    text: "",
+                    innerHTML: "",
+                    direction:"up"
+                },
+                {
+                    start: 416,
+                    target: 'caption',
+                    title: '<a href="#/bitcoin-mining">Bitcoin Mining</a>',
+                    text: "",
+                    innerHTML: "",
+                    direction:"up"
+                }
+            ]
         }
+
     ]
     return topics;
 });
