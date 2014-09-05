fdc862bcfdb5de10a4169558e2a493e85160b0e2
Sankalp Kulshreshtha <sankalp221@gmail.com>
2014-09-04 22:28:37 -0400

    CSS tweaks

--- js/angular.main.js
+++ js/angular.main.js
@@ -505,8 +505,8 @@ jadyuApp.controller('TopicCtrl', function ($scope, $http, $filter, Data, $locati
     }
 
     $scope.addAlert = function() {
-        $scope.alerts.push({msg: 'Thanks for your contribution! Your video will be voted on by the community. (Not really, this is a demo)'});
-        console.log("Alert added");
+        if($scope.alerts.length===0)
+            $scope.alerts.push({msg: 'Thanks for your contribution! Your video will be voted on by the community. (Not really, this is a demo)'});
     };
 
     $scope.closeAlert = function(index) {
@@ -518,7 +518,6 @@ jadyuApp.controller('TopicCtrl', function ($scope, $http, $filter, Data, $locati
     $scope.topics = Data;
     $scope.alerts = [];
     $scope.isAdvanced=true;
-    $scope.states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Dakota', 'North Carolina', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
 
     /* Watches */
     /* ------------------------ */
